e35f4477a18c852317c4dd71a5c0fc9bf26ec5a2
Priyanka Agrawal <agrawalpriyanka1612@gmail.com>
2024-06-27 19:15:54 +0530

    question9

---
+++ name_list_9.py
@@ -0,0 +1,12 @@
+details = input("enter your first name , last name and age")
+list1 = details.split(',')
+first_name = list1[0]
+last_name = list1[1]
+age = list1[2]
+
+print("My first name is",first_name,"and my last name is",last_name,"and",first_name,"is",age,"year old")
+
+
+
+
+
